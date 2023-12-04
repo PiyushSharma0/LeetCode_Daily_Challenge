6e42227f9c31a18458cf8b42438bf7de4dc79da8
Piyush Sharma <piyush.sharma.code@gmail.com>
2023-12-04 23:51:17 +0530

    Day 187

---
+++ 12_December2023/04_12_Largest_3_Same_Digit_Number_in_a_String.java
@@ -0,0 +1,33 @@
+// Link: https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/
+// Difficulty: Easy
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        if (num.indexOf("999") != -1) {
+            return "999";
+        } else if (num.indexOf("888") != -1) {
+            return "888";
+        } else if (num.indexOf("777") != -1) {
+            return "777";
+        } else if (num.indexOf("666") != -1) {
+            return "666";
+        } else if (num.indexOf("555") != -1) {
+            return "555";
+        } else if (num.indexOf("444") != -1) {
+            return "444";
+        } else if (num.indexOf("333") != -1) {
+            return "333";
+        } else if (num.indexOf("222") != -1) {
+            return "222";
+        } else if (num.indexOf("111") != -1) {
+            return "111";
+        } else if (num.indexOf("000") != -1) {
+            return "000";
+        }
+
+        return "";
+        
+    }
+}
