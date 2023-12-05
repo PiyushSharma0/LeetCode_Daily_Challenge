35fb4e6548527811877c03f19cbca0dcb3af4d49
Piyush Sharma <piyush.sharma.code@gmail.com>
2023-12-05 19:21:26 +0530

    Day 188

---
+++ 12_December2023/05_12_Count_of_Matches_in_a_Tournament.java
@@ -0,0 +1,10 @@
+// Link: https://leetcode.com/problems/count-of-matches-in-tournament/
+// Difficulty: Easy
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+class Solution {
+    public int numberOfMatches(int n) {
+        return n-1;
+    }
+}
