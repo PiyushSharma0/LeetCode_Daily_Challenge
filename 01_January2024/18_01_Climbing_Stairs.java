fc054c601984947f4bbca594832cf4dc089c500c
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-01-18 19:09:12 +0530

    Day 232

---
+++ 01_January2024/18_01_Climbing_Stairs.java
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/climbing-stairs/
+// Difficulty: Easy
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; ++i){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
