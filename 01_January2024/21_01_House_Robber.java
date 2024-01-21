f8019caa16e4892d71e589ba31cd0c30039a1ccd
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-01-21 22:28:15 +0530

    Day 235

---
+++ 01_January2024/21_01_House_Robber.java
@@ -0,0 +1,18 @@
+// Link: https://leetcode.com/problems/house-robber/
+// Difficulty: Medium but easy
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+    public int rob(int[] nums) {
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);
+    }
+}
