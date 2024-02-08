c0fb7b0aca21ef5649c903b9d5b0d59e2aeec463
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-02-08 22:51:38 +0530

    Day 253

---
+++ 02_February2024/08_02_Perfect_Squares.java
@@ -0,0 +1,27 @@
+// Link: https://leetcode.com/problems/perfect-squares/
+// Difficulty: Medium
+// Time complexity: O(sqrt(n))
+// Space complexity: O(1)
+
+class Solution {
+    public int numSquares(int n) {
+         int sqrt = (int) Math.sqrt(n);
+
+        if (sqrt * sqrt == n)
+            return 1;
+
+        while (n % 4 == 0)
+            n = n / 4;
+
+        if (n % 8 == 7) return 4;
+
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+            int base = (int) Math.sqrt(n - square);
+
+            if (base * base == n - square) return 2;
+        }
+
+        return 3;
+    }
+}
