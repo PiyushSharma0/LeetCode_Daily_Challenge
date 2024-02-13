5fc17a8318aff848c34ad5d73283cc34a38dd811
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-02-13 23:53:43 +0530

    Day 258

---
+++ 02_February2024/13_02_First_Palindromic_String_in_a_Sentence.java
@@ -0,0 +1,27 @@
+// Link: https://leetcode.com/problems/first-palindromic-string-in-a-sentence/
+// Difficulty: Easy
+// Time Complexity: O(n * m)
+// Space Complexity: O(1)
+
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
