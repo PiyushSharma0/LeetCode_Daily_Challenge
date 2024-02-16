b3eab483ef3b2ef9208398249257f317a1116f68
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-02-16 23:44:11 +0530

    Day 260

---
+++ 02_February2024/15_02_Find_Polygon_with_Largest_Perimeter.java
@@ -0,0 +1,23 @@
+// Link: https://leetcode.com/problems/largest-perimeter-triangle/
+// Difficulty: Easy
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+
+import java.util.Arrays;
+
+class Solution {
+    public long largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        long sum = 0;
+        long ans = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < sum) {
+                ans = nums[i] + sum;
+            }
+            sum += nums[i];
+        }
+        
+        return ans;
+    }
+}
