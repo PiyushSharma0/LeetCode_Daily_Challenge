c9e4e5bbbe02858f129bb7e6db9249fa4d4dfd14
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-02-21 20:51:43 +0530

    Day 267

---
+++ 02_February2024/21_02_Bitwise_AND_of_Number_Range.java
@@ -0,0 +1,20 @@
+// Link: https://leetcode.com/problems/bitwise-and-of-numbers-range/
+// Difficulty: Medium
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        
+        // Find the common leftmost bits
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        
+        // Left shift back to restore the result
+        return left << shift;
+    }
+}
