e6fa6048c2bcf817c85d8e7ed82f3e723ef2046a
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-02-24 04:02:33 +0530

    Day 269

---
+++ 02_February2024/23_02_Cheapest_Flights_within_K_Stops.java
@@ -0,0 +1,34 @@
+// Link: https://leetcode.com/problems/cheapest-flights-within-k-stops/
+// Difficulty: Medium
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+         int[] distance = new int[n];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[src] = 0;
+        for (int i = 0; i <= k; i++) {
+            if (isRoutePossible(distance, flights)) {
+                break;
+            }
+        }
+        return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
+    }
+    private boolean isRoutePossible(int[] dist, int[][] flights) {
+        int[] copy = Arrays.copyOf(dist, dist.length);
+        boolean result = true;
+
+        for (int[] flight : flights) {
+            int src = flight[0];
+            int dst = flight[1];
+            int  cost =flight[2];
+
+            if (copy[src] < Integer.MAX_VALUE && dist[dst] > dist[src] + cost) {
+                dist[dst] = cost + copy[src];
+                result = false;
+            }
+        }
+        return result;
+    }
+}
