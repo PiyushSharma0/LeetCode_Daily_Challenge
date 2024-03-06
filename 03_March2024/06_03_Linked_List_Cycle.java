c590927c9d06662e292e9c7430125e49f372cdc0
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-03-06 22:34:57 +0530

    Day 280

---
+++ 03_March2024/06_03_Linked_List_Cycle.java
@@ -0,0 +1,36 @@
+// Link: https://leetcode.com/problems/linked-list-cycle/
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
