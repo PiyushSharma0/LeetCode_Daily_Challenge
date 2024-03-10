69c8e04e6084ea612a23a1a11f98c1f4db2870e9
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-03-10 22:42:30 +0530

    Day 283

---
+++ 03_March2024/09_03_Minimum_Common_Value.java
@@ -0,0 +1,20 @@
+// Link: https://leetcode.com/problems/minimum-common-value/
+// Difficulty: Easy
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
